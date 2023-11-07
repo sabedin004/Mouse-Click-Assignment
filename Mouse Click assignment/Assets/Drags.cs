eff654cda1eddea2b4ec20f98ec3934d40ea31f7
sabedin004 <144362658+sabedin004@users.noreply.github.com>
2023-11-07 12:19:23 -0500

    Mouse Click assignments

---
+++ Mouse Click assignment/Assets/Drags.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drags : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left Click");
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Right click");
+        }
+        if (Input.GetMouseButtonDown(2))
+        {
+            Debug.Log("Middle Click");
+        }
+
+
+    }
+}
